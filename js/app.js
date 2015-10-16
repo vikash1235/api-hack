759ecade1cc808501de62e2d8905ea1a78efbace
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-10-15 17:11:51 -0700

    Created validation function for the inputs. One validation scenario to be dealt with(space).

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
 $(document).ready(function() {
 	$('#searchButton').on('click', function() {
 		var jobName = $('#job-title').val();
-		var jobLocation = $('#job-locations').val();
-		if ((jobValidation(jobName) == true) && (jobValidation(jobLocation) == true)) {
+		var jobLocation = document.getElementById('job-location').value;
+		if (jobValidation(jobName, jobLocation) == true) {
 			searchJobs(jobName, jobLocation);
 		} else {
-			alert('Please enter a valid job title!');
-			return false;
-			$(jobName).val('');
+			$('#job-title').val('');
+			//jobLocation.value == 'choose location';
+			return false;	
 		}
 	});
 
-	function jobValidation(input) {
+	// Function to validate the input provided to the text box and drop down list
+	function jobValidation(jobName, jobLocation) {
 		var checkInput = true;
-		if ((input == '') || (input == ' ') || (!/[^a-zA-Z]/.test(input))) {
+		if ((jobName == '') || (jobName == ' ') || (/[^a-zA-Z]/.test(jobName))) {
+			alert('Please enter a valid job title!');
+			checkInput = false;
+		} 
+		if (jobLocation == 'choose location') {
+			alert('Please select a desired location.');
 			checkInput = false;
 		}
 		return checkInput;
+		//console.log(checkInput);
 	}
+
+	/* Function to make AJAX request */
 
 	function jobDisplay(output) {
 
 	}
 
 	function searchJobs(jobName, jobLocation) {
-
+		alert("So far, so good");
 	}
 });
