87247b955f8a57867e554b9fb8db4c757b317b44
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-10-29 01:32:49 -0700

    1. Changed the project from LinkedIn to Recipe search(Yummly) due to API limitations. 2. Added the functionality. 3. Responsive design done.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,55 @@
-$(document).ready(function() {
-	$('#searchButton').on('click', function() {
-		var jobName = $('#job-title').val();
-		var jobLocation = document.getElementById('job-location').value;
-		if (jobValidation(jobName, jobLocation) == true) {
-			searchJobs(jobName, jobLocation);
-		} else {
-			$('#job-title').val('');
-			//jobLocation.value == 'choose location';
-			return false;	
-		}
+$(document).ready( function() {
+	$('#searchButton').on('click', function (event) {
+	    event.preventDefault();
+	    var keyword = $('#ingredient').val();
+	    var cuisine = $('#cuisine-name').val();
+	    recipeValidation(keyword,cuisine);
 	});
-
-	// Function to validate the input provided to the text box and drop down list
-	function jobValidation(jobName, jobLocation) {
-		var checkInput = true;
-		if ((jobName == '') || (jobName == ' ') || (/[^a-zA-Z]/.test(jobName))) {
-			alert('Please enter a valid job title!');
-			checkInput = false;
-		} 
-		if (jobLocation == 'choose location') {
-			alert('Please select a desired location.');
-			checkInput = false;
-		}
-		return checkInput;
-		//console.log(checkInput);
-	}
-
-	/* Function to make AJAX request */
-
-	function jobDisplay(output) {
-
-	}
-
-	function searchJobs(jobName, jobLocation) {
-		alert("So far, so good");
-	}
 });
+
+var recipeValidation = function(keyword,cuisine) {
+	if ((keyword == '') && (cuisine == null)) {
+	   	alert('Please enter something in the text box and try again!');
+	    $('.recipe-details').html('');
+	    return false;
+	} else {
+		getRecipe(keyword,cuisine);
+	}
+}
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+}
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getRecipe = function(keyword,cuisine) {
+	
+	var result = $.ajax({
+		url: "http://api.yummly.com/v1/api/recipes?_app_id=6d9e22ab&_app_key=e4270a20949b90bf9cca1017d935f12b&q=" + keyword + "&allowedCuisine[]=cuisine^cuisine-" + cuisine + "&requirePictures=true",
+		dataType: "jsonp",
+		type: "GET"
+		})
+	.done(function(result){
+		$('.recipe-details').html('');
+		$.each(result.matches, function(i, matches) {
+			var recipe = '<li><div class="recipe-image"><img src="' +
+							matches.imageUrlsBySize[90] + '" alt="Recipe image" width="170"></div><div class="recipe-description"><p>' +
+							matches.sourceDisplayName + '</p><p><a target="_blank" href=https://www.yummly.com/recipe/' +
+							matches.id + ' >' + matches.recipeName + '</a></p><p>Cooking time: ' + 
+							matches.totalTimeInSeconds/60 + ' minutes</p><p>Rating: ' + 
+							matches.rating + '</p></div></li>';
+			$('.recipe-details').append(recipe);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+}
+
+
+
